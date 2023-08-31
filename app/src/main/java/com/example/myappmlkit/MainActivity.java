03592fb4b1d4080c273a5189b5d7c06a3bc2a525
WilliamLex <willyorrala8@gmail.com>
2023-08-31 14:57:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myappmlkit/MainActivity.java
@@ -0,0 +1,210 @@
+package com.example.myappmlkit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.Manifest;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceContour;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnSuccessListener<Text>,
+        OnFailureListener {
+    Bitmap mSelectedImage;
+    ImageView mImageView;
+    ArrayList<String> permisosNoAprobados;
+    Button btnCamara;
+    Button btnGaleria;
+    TextView txtResults;
+    public static int REQUEST_CAMERA = 111;
+    public static int REQUEST_GALLERY = 222;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayList<String> permisos_requeridos = new ArrayList<String>();
+        permisos_requeridos.add(Manifest.permission.CAMERA);
+        permisos_requeridos.add(Manifest.permission.MANAGE_EXTERNAL_STORAGE);
+        permisos_requeridos.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        mImageView= findViewById(R.id.image_view);
+        txtResults = findViewById(R.id.txtresults);
+        btnCamara = findViewById(R.id.btCamera);
+        btnGaleria=  findViewById(R.id.btGallery);
+        permisosNoAprobados  = getPermisosNoAprobados(permisos_requeridos);
+        requestPermissions(permisosNoAprobados.toArray(new String[permisosNoAprobados.size()]),
+                100);
+    }
+
+    public void abrirGaleria (View view){
+        Intent i = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, REQUEST_GALLERY);
+    }
+
+    public void abrirCamera (View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && null != data) {
+            try {
+                if (requestCode == REQUEST_CAMERA)
+                    mSelectedImage = (Bitmap) data.getExtras().get("data");
+                else
+                    mSelectedImage = MediaStore.Images.Media.getBitmap(getContentResolver(), data.getData());
+                mImageView.setImageBitmap(mSelectedImage);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for(int i=0; i<permissions.length; i++){
+            if(permissions[i].equals(Manifest.permission.CAMERA)){
+                //btnCamara.setEnabled(grantResults[i] == PackageManager.PERMISSION_GRANTED);
+            } else if(permissions[i].equals(Manifest.permission.MANAGE_EXTERNAL_STORAGE) ||
+                    permissions[i].equals(Manifest.permission.READ_EXTERNAL_STORAGE)
+            ) {
+                //btnGaleria.setEnabled(grantResults[i] == PackageManager.PERMISSION_GRANTED);
+            }
+        }
+    }
+
+    public ArrayList<String> getPermisosNoAprobados(ArrayList<String>  listaPermisos) {
+        ArrayList<String> list = new ArrayList<String>();
+        Boolean habilitado;
+
+
+        if (Build.VERSION.SDK_INT >= 23)
+            for(String permiso: listaPermisos) {
+                if (checkSelfPermission(permiso) != PackageManager.PERMISSION_GRANTED) {
+                    list.add(permiso);
+                    habilitado = false;
+                }else
+                    habilitado=true;
+
+                if(permiso.equals(Manifest.permission.CAMERA))
+                    btnCamara.setEnabled(habilitado);
+                else if (permiso.equals(Manifest.permission.MANAGE_EXTERNAL_STORAGE)  ||
+                        permiso.equals(Manifest.permission.READ_EXTERNAL_STORAGE))
+                    btnGaleria.setEnabled(habilitado);
+            }
+
+
+        return list;
+    }
+
+    @Override
+    public void onFailure(@NonNull Exception e) {
+
+    }
+
+    public void OCRfx(View v) {
+        InputImage image = InputImage.fromBitmap(mSelectedImage, 0);
+        TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+        recognizer.process(image)
+                .addOnSuccessListener(this)
+                .addOnFailureListener(this);
+    }
+
+    @Override
+    public void onSuccess(Text text) {
+
+        List<Text.TextBlock> blocks = text.getTextBlocks();
+        String resultados="";
+        if (blocks.size() == 0) {
+            resultados = "No hay Texto";
+        }else{
+            for (int i = 0; i < blocks.size(); i++) {
+                List<Text.Line> lines = blocks.get(i).getLines();
+                for (int j = 0; j < lines.size(); j++) {
+                    List<Text.Element> elements = lines.get(j).getElements();
+                    for (int k = 0; k < elements.size(); k++) {
+                        resultados = resultados + elements.get(k).getText() + " ";
+                    }
+                }
+            }
+            resultados=resultados + "\n";
+        }
+        txtResults.setText(resultados);
+    }
+
+    public void Rostrosfx(View v) {
+
+        ImageView imagen=findViewById(R.id.image_view);
+        BitmapDrawable drawable=(BitmapDrawable) imagen.getDrawable();
+        Bitmap bitmap=drawable.getBitmap().copy(Bitmap.Config.ARGB_8888, true);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint= new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(5);
+        paint.setTextSize(80);
+
+        InputImage image = InputImage.fromBitmap(mSelectedImage, 0);
+        FaceDetectorOptions options =
+                new FaceDetectorOptions.Builder()
+                        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+                        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+                        .build();
+        FaceDetector detector = FaceDetection.getClient(options);
+        detector.process(image)
+                .addOnSuccessListener(new OnSuccessListener<List<Face>>() {
+                    @Override
+                    public void onSuccess(List<Face> faces) {
+                        if (faces.size() == 0) {
+                            txtResults.setText("No Hay rostros");
+                        }else{
+                            txtResults.setText("Hay " + faces.size() + " rostro(s)");
+                        }
+                        for (Face face: faces){
+                            int left=face.getBoundingBox().left;
+                            int top=face.getBoundingBox().top;
+                            int right=face.getBoundingBox().right;
+                            int bottom=face.getBoundingBox().bottom;
+                            Rect rect = new Rect(left, top, right, bottom);
+                            canvas.drawRect(rect, paint);
+                        }
+                        imagen.setImageBitmap(bitmap);
+                    }
+                })
+                .addOnFailureListener(this);
+    }
+}
